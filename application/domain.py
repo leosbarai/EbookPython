4e1223ee445c18d3356eeb7158e2255fdd213185
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2021-02-17 14:31:15 -0300

    Método estático.

--- application/domain.py
+++ application/domain.py
@@ -52,6 +52,9 @@ class DataTable:
         self._referenced.append(relationship)
 
 
+from decimal import Decimal
+
+
 class Column:
     """ Representa uma coluna em um DataTable
 
@@ -61,11 +64,11 @@ class Column:
 
         Attributes:
             name: Nome da Coluna
-            king: Tipo do Dado (varchar, bigint, numeric)
+            kind: Tipo do Dado (varchar, bigint, numeric)
             description: Descrição da coluna
     """
 
-    def __init__(self, name, kind, descripton=""):
+    def __init__(self, name, kind, description=""):
         """ Construtor:
 
             Args:
@@ -75,7 +78,44 @@ class Column:
         """
         self._name = name
         self._kind = kind
-        self._description = descripton
+        self._description = description
+
+    def __str__(self):
+        _str = "Col: {} : {} {}".format(self._name,
+                                        self._kind,
+                                        self._description)
+        return _str
+
+    def _validate(kind, data):
+        if kind == 'bigint':
+            if isinstance(data, int):
+                return True
+            return False
+        elif kind == 'varchar':
+            if isinstance(data, str):
+                return True
+            return False
+        elif kind == 'numeric':
+            try:
+                val = Decimal(data)
+            except:
+                return False
+            return True
+
+    validate = staticmethod(_validate)
+
+
+class PrimaryKey(Column):
+    def __init__(self, table, name, kind, description=""):
+        super().__init__(name, kind, description=description)
+        self._is_pk = True
+
+    def __str__(self):
+        _str = "Col: {} : {} {}".format(self._name,
+                                        self._kind,
+                                        self._description)
+
+        return "{} - {}".format('PK', _str)
 
 
 class Relationship:
@@ -98,3 +138,80 @@ class Relationship:
         self._from = _from
         self._to = to
         self._on = on
+
+
+# coding: utf-8
+
+import os
+
+
+def extract_name(name):
+    return name.split(".")[0]
+
+
+def read_lines(filename):
+    _file = open(os.path.join("data/meta-data", filename), "rt")
+    data = _file.read().split("\n")
+    _file.close()
+    return data
+
+
+def read_metadata(filename):
+    metadata = []
+    for column in read_lines(filename):
+        if column:
+            metadata.append(tuple(column.split('\t')[:3]))
+    return metadata
+
+
+def prompt():
+    print("\nO que deseja ver?")
+    print("(l) Listar entidades")
+    print("(d) Exibir atributos de uma entidade")
+    print("(r) Exibir referências de uma entidade")
+    print("(s) Sair do programa")
+    return input('')
+
+
+def main():
+    # dicionário nome entidade -> atributos
+    meta = {}
+    # dicionário identificador -> nome entidade
+    keys = {}
+    # dicionário de relacionamentos
+
+    relationships = {}
+    for meta_data_file in os.listdir("data/meta-data"):
+        table_name = extract_name(meta_data_file)
+        attributes = read_metadata(meta_data_file)
+        identifier = attributes[0][0]
+
+        meta[table_name] = attributes
+        keys[identifier] = table_name
+
+    for key, val in meta.items():
+        for col in val:
+            if col[0] in keys:
+                if not col[0] == meta[key][0][0]:
+                    relationships[key] = keys[col[0]]
+
+    opcao = prompt()
+    while opcao != 's':
+        if opcao == 'l':
+            for entity_name in meta.keys():
+                print(entity_name)
+        elif opcao == 'd':
+            entity_name = input('Nome da entidade: ')
+            for col in meta[entity_name]:
+                print(col)
+        elif opcao == 'r':
+            entity_name = input('Nome da entidade: ')
+            other_entity = relationships[entity_name]
+            print(other_entity)
+        else:
+            print("Inexistente\n")
+        opcao = prompt()
+
+
+if __name__ == '__main__':
+    main()
